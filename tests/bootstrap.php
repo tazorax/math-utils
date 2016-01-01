241b2377a6742f92f51ca2480dbcb03b1b997389
tazorax <tazorax@gmail.com>
2016-01-01 15:45:24 +0100

    Update PHPDoc

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tazo
- * Date: 25/11/2015
- * Time: 23:20
+/*
+ * This file is part of the MathUtils package.
+ *
+ * (c) tazorax <tazorax@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 if ( ! is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
